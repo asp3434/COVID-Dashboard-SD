ade4fd67eb049573a3bb7084839d367bb3794d4d
Tyler Silva <tyler.silva.ut@gmail.com>
2022-12-08 15:00:39 -0700

    simplified DataDisplayCurrent

--- DataDisplayCurrent.py
+++ DataDisplayCurrent.py
@@ -1,55 +1,32 @@
 
-# This script takes data from the json files for a certain country and then plots any of the four statistics
+# This script lets user choose the contries and statistic desired and then plots the data
 
 # NEED:
-# 1. Allow user to pick country and pick a stat for the country --> then plot stat over time (working in script, need UI for bokeh)
-# 2. Allow user to compare multiple plots for country stats over time on the same graph  (need in script and bokeh)
+# 1. In UI - Allow user to pick country and pick a stat for the country --> then plot stat over time 
+# 2. In UI - Allow user to compare multiple plots for country stats over time on the same graph  
 
-# Pseudocode for adding multiple plots (just in script, no widgets):
-# Initialize the figure
-# for country in countries:
-#       execute the main part of the script
-#       and add the stat to the plot
-
-#import modules
-from bokeh.models import DatetimeTickFormatter, NumeralTickFormatter, widgets, Select
-from bokeh.plotting import figure, show, curdoc
-from bokeh.layouts import grid, gridplot, row, layout
-from bokeh.events import MenuItemClick, Event
-from ScrapeWebsite import scrape_country
-from bokeh.models import CustomJS, MultiSelect
+# import bokeh modules
+from bokeh.models import DatetimeTickFormatter
+from bokeh.plotting import figure, show
 from bokeh.io import show
-from bokeh.models import CustomJS, TextInput
-from bokeh.models import DatetimeTickFormatter, NumeralTickFormatter
-from bokeh.io import output_file, show
-from bokeh.models.widgets import Select
+from bokeh.models import DatetimeTickFormatter
 from bokeh.colors import RGB
+
+# import other modules
 import json
-import numpy 
 import random
-import string
-
-# Date modules
 import datetime
-from datetime import date
-from datetime import timedelta, datetime
-from datetime import datetime, timedelta
-
-colors = ['blue', 'green', 'red']
+from datetime import timedelta, datetime, date
 
 # user input: countries and stat
-countries = ["usa", "brazil", "france"]
+countries = ['usa', 'brazil', 'japan']
 stats_choices = ['total deaths', '(total deaths)/1m', 'daily deaths', '(daily deaths)/1m']
 stat_choice = stats_choices[3]
 
 # create new plot
 p = figure(title=stat_choice + " vs time", 
-    x_axis_label='date', y_axis_label=stat_choice, x_axis_type="datetime",
+    x_axis_label='date mm/yy', y_axis_label=stat_choice, x_axis_type="datetime",
     sizing_mode="stretch_width",max_width=700,height=400,)
-
-
-# for color indexing
-i = 0
 
 for country in countries:
     # initialize stat lists
@@ -68,9 +45,9 @@ for country in countries:
     country_data = []
     # number of json files that are not available (were skipped)
     times_skipped = 1
+
     # Loop through each date and make arrays for each of the four stats over time
     while start_date <= end_date:
-        # use this if want to print the dates --> print(start_date.strftime("%Y-%m-%d"))
         try:
             with open('country_statistics1_'+str(start_date)+'.json') as f:
                 # get country data
@@ -85,6 +62,7 @@ for country in countries:
         tot_deaths_1m.append(country_data[1])
         daily_deaths.append(country_data[2])
         daily_deaths_1m.append(country_data[3])
+
         # convert dates to datetimes so bokeh dateticks on the x axis will work (this is for plotting only)
         dateString = str(start_date)
         dateList = dateString.split('-')
@@ -94,6 +72,7 @@ for country in countries:
         # increment the day
         start_date += delta
 
+        # generate a random color for each country being plotted
         x1 = random.random()
         x2 = random.random()
         x3 = random.random()
@@ -103,18 +82,16 @@ for country in countries:
         color_rand = RGB(r = red,
             g = green,
             b = blue)
-        # add line
+        
+        # dictionary to convert user input into the array containing the data for the user selected statistic
         stats_dict = {'total deaths' : tot_deaths, '(total deaths)/1m' : tot_deaths_1m, 
         '(daily deaths)' : daily_deaths, '(daily deaths)/1m' : daily_deaths_1m}
 
+        # add line
         p.line(all_dates, stats_dict[stat_choice], legend_label=country,color=color_rand, line_width=1)
 
         # format x axis date ticks
         p.xaxis[0].formatter = DatetimeTickFormatter(days="%m / %d")
-    i += 1
         
-
-
-# outside loop
-# # show the results
+# show the results in a new tab
 show(p)
