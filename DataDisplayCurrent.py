fee0f384e05af78b1131c3517f592f413c0a5ecb
Andrew Prescott <andrew.prescott.2019@gmail.com>
2022-12-09 11:39:34 -0700

    Changes to datadisplay, grab all countries

--- DataDisplayCurrent.py
+++ DataDisplayCurrent.py
@@ -8,8 +8,13 @@
 from bokeh.models import DatetimeTickFormatter
 from bokeh.plotting import figure, show
 from bokeh.io import show
-from bokeh.models import DatetimeTickFormatter
+from bokeh.models import (ColumnDataSource, DataTable, HoverTool, IntEditor,
+                          NumberEditor, NumberFormatter, SelectEditor,
+                          StringEditor, StringFormatter, TableColumn)
 from bokeh.colors import RGB
+from bokeh.layouts import column, row
+
+import pandas as pd
 
 # import other modules
 import json
@@ -18,7 +23,12 @@ import datetime
 from datetime import timedelta, datetime, date
 
 # user input: countries and stat
-countries = ['usa', 'brazil', 'japan', 'russia', 'australia', 'france']
+f = open('country_statistics1_2022-11-28.json')
+data1 = json.load(f)
+
+countries = list(data1.keys())
+
+# countries = ['usa', 'brazil', 'japan', 'russia', 'australia', 'france']
 stats_choices = ['total deaths', '(total deaths)/1m', 'daily deaths', '(daily deaths)/1m']
 stat_choice = stats_choices[1]
 
@@ -26,6 +36,12 @@ stat_choice = stats_choices[1]
 p = figure(title=stat_choice + " vs time", 
     x_axis_label='date mm/yy', y_axis_label=stat_choice, x_axis_type="datetime",
     sizing_mode="stretch_width",max_width=700,height=400,)
+
+# data frame arrays
+df_total_deaths_ = []
+df_total_deaths_1m = []
+df_daily_deaths = []
+df_daily_deaths_1m = []
 
 for country in countries:
     # initialize stat lists
@@ -82,19 +98,41 @@ for country in countries:
             g = green,
             b = blue)
         
+        
+        
         # dictionary to convert user input into the array containing the data for the user selected statistic
         stats_dict = {'total deaths' : tot_deaths, '(total deaths)/1m' : tot_deaths_1m, 
         'daily deaths' : daily_deaths, '(daily deaths)/1m' : daily_deaths_1m}
 
         # add line
-        p.line(all_dates, stats_dict[stat_choice], legend_label=country,color=color_rand, line_width=1)
+        # p.line(all_dates, stats_dict[stat_choice], legend_label=country,color=color_rand, line_width=1)
+        p.line(all_dates, stats_dict[stat_choice],color=color_rand, line_width=1)
 
         # format x axis date ticks
         p.xaxis[0].formatter = DatetimeTickFormatter(days="%m / %d")
+    
+    
+df_dict = {}
 
-p.legend.click_policy="hide"
+# Create table
+# source = ColumnDataSource(pd.DataFrame
 
-#p.add_layout(mytext)
+# columns = [
+#     TableColumn(field="country", title="Country",
+#                 # editor=SelectEditor(options=manufacturers),
+#                 formatter=StringFormatter(font_style="bold")),
+#     TableColumn(field="total_deaths", title="Total Deaths", editor=IntEditor()),
+#     TableColumn(field="new_deaths_daily", title="Daily New Deaths", editor=IntEditor()),
+#     TableColumn(field="deaths_per_mil", title="Total Deaths per Million", editor=IntEditor()),
+#     TableColumn(field="deaths_per_mil_daily", title="Daily Deaths per Million", editor=IntEditor()),
+# ]
+# data_table = DataTable(source=source, columns=columns, editable=True, width=800,
+#                        index_position=-1, index_header="row index", index_width=60)
+
+# p.legend.click_policy="hide"
+
+# p.add_layout(mytext)
 
 # show the results in a new tab
+# show(column(p, data_table))
 show(p)
